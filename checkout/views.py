b405a2f33687d8916d20d7451c2ce692a75c2187
Kevin John Kiely <kevinjohnkiely1@hotmail.com>
2022-08-15 15:30:57 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LX4zODAMQP2OTL4ScGSGU2wRlx0xraGmsDEoPCX3dszn1ba5m7jflLONqMmM9O5DHBaiWsRbG6h3xKJUxcbxIki00MIJX557i',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
